1a285276b027c0435f7cc2a9c78376d7f509af72
jacordel <jandrescordel@gmail.com>
2021-05-28 16:37:22 -0700

    complete

--- scripts/index.js
+++ scripts/index.js
@@ -10,28 +10,29 @@ const popupEl = document.querySelector('.popup');
 const profileNameEl = document.querySelector('.profile__name');
 const profileProfessionEl = document.querySelector('.profile__profession');
 
+
 //enables popup to open
 function openPopup() {
   popupEl.classList.add('popup_open');
+  //enables input values to be displayed as the profile data
+  editFormNameInput.value = profileNameEl.textContent;
+  editFormAboutMeInput.value = profileProfessionEl.textContent;
 }
 profileEditBtnEl.addEventListener('click', openPopup);
 
+//enables popup to close
 function closePopup() {
   popupEl.classList.remove('popup_open');
 }
 popupCloseBtnEl.addEventListener('click', closePopup);
-
-//enables input values to be displayed as the profile data
-editFormNameInput.value = profileNameEl.textContent;
-editFormAboutMeInput.value = profileProfessionEl.textContent;
 
 //enables profile data to equal input value of popup
 function handleFormSubmit(evt) {
   evt.preventDefault();
   profileNameEl.textContent = editFormNameInput.value;
   profileProfessionEl.textContent = editFormAboutMeInput.value;
-  closePopup();
-};
+  closePopup(); 
+}; 
 editFormEl.addEventListener('submit', handleFormSubmit);
 
 
